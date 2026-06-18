5018a7eb2ca767b067ca89f971544e8d995648fa
Harinee133 <harinee.balaji@my.jcu.edu.au>
2026-06-18 10:41:53 +0800

    Create ExpenseCategory enum for transaction classification

---
+++ app/src/main/java/au/edu/jcu/cp3406_cp5307_utilityappstartertemplate/Expense.kt
@@ -0,0 +1,9 @@
+package au.edu.jcu.cp3406_cp5307_utilityappstartertemplate
+
+enum class ExpenseCategory(val displayName: String) {
+    Transportation("Transportation"),
+    Food("Food & Dining"),
+    Entertainment("Entertainment"),
+    Utilities("Utilities"),
+    Others("Others")
+}
